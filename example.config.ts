80503ed43098dec670fc87aad876995ed94fa07d
Andri Þórhallsson <andrithorhalls@gmail.com>
2024-03-23 22:11:57 +0000

    update example config

---
+++ example.config.ts
@@ -0,0 +1,14 @@
+import { type StoreConfig } from './types';
+
+const config: StoreConfig[] = [
+  {
+    storeName: 'store1',
+    feedUrl: 'https://url.to/shopping-feed-1.xml'
+  },
+  {
+    storeName: 'store2',
+    feedUrl: 'https://url.to/shopping-feed-2.xml'
+  }
+];
+
+export default config;
